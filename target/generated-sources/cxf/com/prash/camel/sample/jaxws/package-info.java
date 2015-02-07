9018213fe3062a32218fc0aadd340085a909dec9
mekaprashanth <mekaprashanth@gmail.com>
2015-02-08 00:51:08 +0530

    Delete target

--- target/generated-sources/cxf/com/prash/camel/sample/jaxws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://jaxws.sample.camel.prash.com/")
-package com.prash.camel.sample.jaxws;
--- target/generated-sources/cxf/com/prash/camel/sample/jaxws/values/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://values.jaxws.sample.camel.prash.com/")
-package com.prash.camel.sample.jaxws.values;
